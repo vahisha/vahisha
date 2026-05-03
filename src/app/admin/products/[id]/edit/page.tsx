a83cb4f40bbf9f232022821f678f20d2af88acc7
ARKIO Developer <dev@arkio.ai>
2026-05-03 13:16:45 +0530

    Fix build: Add 'use client' directive to dynamic import pages

--- src/app/admin/products/[id]/edit/page.tsx
+++ src/app/admin/products/[id]/edit/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import dynamic from 'next/dynamic'
 
 const EditProductForm = dynamic(() => import('./EditProductForm'), { ssr: false })
--- src/app/admin/products/new/page.tsx
+++ src/app/admin/products/new/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import dynamic from 'next/dynamic'
 
 const NewProductForm = dynamic(() => import('./NewProductForm'), { ssr: false })
